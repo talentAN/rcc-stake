976878e1d88ee19b56c2607f2238d16c3137a9bc
anzhen02 <anzhen02@meituan.com>
2025-02-05 14:56:00 +0800

    wip: 调试测试网部署命令

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,7 +1,12 @@
 import type { HardhatUserConfig } from 'hardhat/config';
+import dotenv from 'dotenv';
 import '@nomicfoundation/hardhat-toolbox-viem';
 import '@nomicfoundation/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
+
+dotenv.config();
+
+const { SEPOLIA_RPC_URL, ETH_RPC_URL, PRIVATE_KEY } = process.env; // 读取环境变量
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,6 +16,12 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200 // 设置 runs 值
       }
+    }
+  },
+  networks: {
+    ropsten: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`] // 注意将 PRIVATE_KEY 前加上 "0x"
     }
   }
 };
