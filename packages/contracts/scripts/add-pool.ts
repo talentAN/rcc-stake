dc7d377a5bcb4da8874e36e36fdc71c237b70b2a
anzhen02 <anzhen02@meituan.com>
2025-02-09 12:35:25 +0800

    wip: hardhat依赖版本兼容

--- packages/contracts/scripts/add-pool.ts
+++ packages/contracts/scripts/add-pool.ts
@@ -1,17 +1,23 @@
 const { ethers } = require('hardhat');
 
 // 替换为你的合约地址
-const contractAddress = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
+const contractAddress = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9';
 
 // 替换为合约的 ABI
 const contractABI = [
-  'function addPool(address _stakeTokenAddress,uint256 _poolWeight,uint256 _minDepositAmount,uint256 _unStakeLockedBlocks,bool _withUpdate'
+  'function addPool(address _stakeTokenAddress,uint256 _poolWeight,uint256 _minDepositAmount,uint256 _unStakeLockedBlocks,bool _withUpdate)'
 ];
 async function main() {
-  const contract = await ethers.getContractAt(contractABI, contractAddress);
-  const tx = await contract.addPool();
-  await tx.wait();
-  console.log('Method b was called with value 100');
+  try {
+    const contract = await ethers.getContractAt(contractABI, contractAddress);
+    console.log(contract);
+    return;
+    const tx = await contract.addPool('0x0', 100, 100, 100, false);
+    await tx.wait();
+    console.log('add Pool成功:', tx);
+  } catch (error) {
+    console.log('add 失败:', error);
+  }
 }
 
 main();
